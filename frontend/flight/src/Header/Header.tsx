72f181a9651ae95a66f412b8bf41d16f12ed6e49
Catalin Candea <catalin.candea@theaccessgroup.com>
2021-08-24 00:52:20 +0300

    Header

---
+++ frontend/flight/src/Header/Header.tsx
@@ -0,0 +1,14 @@
+import "./Header.css";
+
+export const Header = () => {
+  return (
+    <div>
+      <div className="col-md-4">
+        <div className="mainButtons">
+          <button className="mainButton"> Marketplace </button>
+          <button className="mainButton"> Artisti </button>
+        </div>
+      </div>
+    </div>
+  );
+};
--- frontend/flight/src/MainPage/MainPage.tsx
+++ frontend/flight/src/MainPage/MainPage.tsx
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Header } from "../Header/Header";
 import "../MainPage/MainPage.css";
+import { UserCard } from "../User/UserCard";
 
 class MainPage extends Component {
-    render() {
-        return (
-            <div>
-                <div></div>
-                <div className="col-md-4">
-                    <div className="mainButtons">
-                        <button className="mainButton"> Marketplace </button>
-                        <button className="mainButton"> Artisti </button>
-                    </div>
-                </div>
-                <div></div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <div></div>
+        <div>
+          <Header />
+        </div>
+        <div></div>
+      </div>
+    );
+  }
 }
 
 export default MainPage;
