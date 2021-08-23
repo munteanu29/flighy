a6e238d0e801970f9d8ceee5402b48c5156eee03
Catalin Candea <catalin.candea@theaccessgroup.com>
2021-08-24 01:04:29 +0300

    Pages state add

--- frontend/flight/src/Header/Header.tsx
+++ frontend/flight/src/Header/Header.tsx
@@ -1,12 +1,33 @@
+import { FC } from "react";
 import "./Header.css";
 
-export const Header = () => {
+export interface HeaderProps {
+  setSelectedPage: any;
+}
+export const Header: FC<HeaderProps> = ({ setSelectedPage }) => {
+  const changePage = (id: any) => {
+    setSelectedPage(id);
+  };
   return (
     <div>
       <div className="col-md-4">
         <div className="mainButtons">
-          <button className="mainButton"> Marketplace </button>
-          <button className="mainButton"> Artisti </button>
+          <button
+            className="mainButton"
+            onClick={() => {
+              changePage(1);
+            }}
+          >
+            Marketplace
+          </button>
+          <button
+            className="mainButton"
+            onClick={() => {
+              changePage(2);
+            }}
+          >
+            Artisti
+          </button>
         </div>
       </div>
     </div>
--- frontend/flight/src/MainPage/MainPage.tsx
+++ frontend/flight/src/MainPage/MainPage.tsx
@@ -1,20 +1,22 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
+import { useEffect } from "react";
 import { Header } from "../Header/Header";
 import "../MainPage/MainPage.css";
-import { UserCard } from "../User/UserCard";
 
-class MainPage extends Component {
-  render() {
-    return (
+const MainPage = () => {
+  const [selectedPage, setSelectedPage] = useState();
+  useEffect(() => {
+    console.log(selectedPage);
+  }, [selectedPage]);
+  return (
+    <div>
+      <div></div>
       <div>
-        <div></div>
-        <div>
-          <Header />
-        </div>
-        <div></div>
+        <Header setSelectedPage={setSelectedPage} />
       </div>
-    );
-  }
-}
+      <div></div>
+    </div>
+  );
+};
 
 export default MainPage;
