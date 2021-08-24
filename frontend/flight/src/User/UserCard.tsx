ed208c69c31a57df4be5677652690fe8d329af2e
Catalin Candea <catalin.candea@theaccessgroup.com>
2021-08-24 19:41:06 +0300

    bla

--- frontend/flight/src/User/UserCard.tsx
+++ frontend/flight/src/User/UserCard.tsx
@@ -1,8 +1,9 @@
 import { FC } from "react";
+import { useHistory } from "react-router-dom";
 import "./Card.css";
 
 export interface UserCardProps {
-  id: number;
+  id: string;
   avatar: string;
   description: string;
   category: string;
@@ -18,23 +19,19 @@ export const UserCard: FC<UserCardProps> = ({
   firstName,
   lastName,
 }) => {
+  let history = useHistory();
+  const handleHistory = () => history.push(`/artist/${id}`);
   return (
-    <div className="container">
-      <div className="card">
-        <div className="card-header">
-          <img
-            // src="https://c0.wallpaperflare.com/preview/483/210/436/car-green-4x4-jeep.jpg"
-            src={avatar}
-            alt="rover"
-          />
-        </div>
-        <div className="card-body">
-          <span className="tag tag-teal">{category}</span>
-          <h4>
-            {firstName} {lastName}
-          </h4>
-          <p>{description}</p>
-        </div>
+    <div className="card" onClick={handleHistory}>
+      <div className="card-header">
+        <img src={avatar} alt={firstName + " " + lastName} />
+      </div>
+      <div className="card-body">
+        <span className="tag tag-teal">{category}</span>
+        <h4>
+          {firstName} {lastName}
+        </h4>
+        <p>{description}</p>
       </div>
     </div>
   );
