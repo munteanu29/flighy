cdd24d45b7b5a89f81bacbc2996bcbd42c6edcf4
Cristian MUNTEANU <cristian_munteanu@infobest.ro>
2021-08-24 17:11:38 +0300

    another backend

---
+++ flightBackend/itec-backend/Entities/ArtistEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace itec_backend.Entities
+{
+    public class ArtistEntity: Entity
+    {
+        public ArtistEntity(string name, string surname, bool deleted, string id):   base(deleted, id)
+        {
+            Name = name;
+            Surname = surname;
+        }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
---
+++ flightBackend/itec-backend/Migrations/20210824140955_add artist.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace itec_backend.Migrations
+{
+    public partial class addartist : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CountryEntities");
+
+            migrationBuilder.CreateTable(
+                name: "ArtistEntities",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Deleted = table.Column<bool>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Surname = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArtistEntities", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ArtistEntities");
+
+            migrationBuilder.CreateTable(
+                name: "CountryEntities",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(767)", nullable: false),
+                    CovidVaccinationDate = table.Column<DateTime>(type: "datetime", nullable: false),
+                    CovidVaccinationRate = table.Column<float>(type: "float", nullable: false),
+                    Deleted = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CountryEntities", x => x.Id);
+                });
+        }
+    }
+}
